f48fab74f95468fc7f734a5dc120847cbbb1ca2b
unknown <sivad_000@PU190041.princeton.edu>
2015-03-08 16:45:06 -0400

    Added sort types (alphabetical vs by date)

--- articles/views.py
+++ articles/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import get_object_or_404, render
 
 from articles.models import Article
 
-def display(request, num_to_display):
-	latest_articles = Article.objects.order_by('-pub_date')[:num_to_display]
-	context = { 'latest_articles': latest_articles, 'num': num_to_display }
+def display(request, num_to_display, display_type):
+	if (display_type == 'Date'):
+		latest_articles = Article.objects.order_by('-pub_date')[:num_to_display]
+	elif (display_type == 'Name'):
+		latest_articles = Article.objects.order_by('title')[:num_to_display]		
+	context = { 'latest_articles': latest_articles, 'num': num_to_display, 'display_type': display_type }
 	return render(request, 'articles/index.html', context)
 
 def index(request):
-	return display(request, 10)
+	return display(request, 10, 'Date')
 
 def change_display(request):
-	num_to_display = int(request.GET.get(['num_to_display'],10))
-	return display(request, num_to_display)
+	num_to_display = int(request.GET.get('num_to_display', 10))
+	display_type = request.GET.get('display_type', None)
+	return display(request, num_to_display, display_type)
 
 def articleid(request, article_id):
 	article = get_object_or_404(Article, pk=article_id)
